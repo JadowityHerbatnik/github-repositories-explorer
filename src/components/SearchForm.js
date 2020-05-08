dfeadf707299b0459bebb903ef575219038c2f90
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-05-08 12:38:41 +0200

    Accessibility

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -19,6 +19,7 @@ const SearchForm = ({ setQuery, fetchData, id, placeholder = defaultPlaceholder 
         value={value}
         onChange={handleChange}
         placeholder={placeholder}
+        aria-label={`${id} input`}
       />
       <button onClick={handleSubmit} className="btn search-btn">
         {searchBtnText}
