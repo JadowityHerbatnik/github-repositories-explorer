01836e4c9af4093b9e3e0468ffac77bb990f830e
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-05-13 21:36:37 +0200

    Added saga

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,16 +2,15 @@ import React from "react";
 //prettier-ignore
 import { fetchStatusType, queryType, usersType, setQueryType, fetchUsersReposType } from "types";
 import { connect } from "react-redux";
-import { setQuery } from "actions";
-import { fetchUsersRepos } from "helpers/fetchUsersRepos";
+import { setQuery, requestUsersRepos } from "actions";
 import SearchForm from "components/SearchForm";
 import SearchResults from "components/SearchResults";
 import UsersList from "containers/UsersList";
 
-function App({ fetchStatus, query, users, setQuery, fetchUsersRepos }) {
+function App({ fetchStatus, query, users, setQuery, requestUsersRepos }) {
   return (
     <div id="app-wrapper">
-      <SearchForm setQuery={setQuery} id="username" fetchData={fetchUsersRepos} />
+      <SearchForm setQuery={setQuery} id="username" fetchData={requestUsersRepos} />
       <SearchResults users={users} fetchStatus={fetchStatus} query={query}>
         <UsersList />
       </SearchResults>
@@ -24,7 +23,7 @@ const mapStateToProps = (state) => ({
   query: state.query,
   users: state.users,
 });
-const mapDispatchToProps = { setQuery, fetchUsersRepos };
+const mapDispatchToProps = { setQuery, requestUsersRepos };
 
 App.propTypes = {
   fetchStatus: fetchStatusType,
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "reducers";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "sagas/";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
   reducers,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
+sagaMiddleware.run(rootSaga);
